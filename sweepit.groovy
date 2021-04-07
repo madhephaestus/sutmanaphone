78e3e9ea41876895414ac2810f74ce06212a3e6b
Kevin Harrington <mad.hephaestus@gmail.com>
2021-04-07 19:56:29 -0400

    add slice loading

--- sweepit.groovy
+++ sweepit.groovy
@@ -1,1 +1,12 @@
-//Your code here
+File f = ScriptingEngine
+	.fileFromGit(
+		"https://github.com/madhephaestus/sutmanaphone.git",//git repo URL
+		"master",//branch
+		"shape.svg.SVG"// File from within the Git repo
+	)
+println "Extruding SVG "+f.getAbsolutePath()
+SVGLoad s = new SVGLoad(f.toURI())
+
+HashMap<String,List<Polygon>> polygonsByLayer = s.toPolygons()
+Polygon myProfile = polygonsByLayer.get("Slice 1").get(0)
+
